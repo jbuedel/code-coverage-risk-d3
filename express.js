30e5bfdf0962c2d02e345e46e669b4724ca8a570
Mark Borcherding <markborcherding@gmail.com>
2012-01-19 02:06:17 +0000

    drop our crappy server

--- express.js
+++ express.js
@@ -6,27 +6,10 @@ app.set('view engine', 'html');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 
-function sendFile(file, response){
-    fs.readFile( __dirname + '/public/' + file, function(error, content) {
-    if (error) {
-        response.writeHead(500);
-        response.end();
-    }
-    else {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.end(content, 'utf-8');
-    }
-  });
-}
 
-app.get('/', function(req, response){
-    sendFile('index.html',response);
+app.configure('development', function(){
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
-
-app.get('/:file', function(req,res){
-  var file = req.params.file;
-  sendFile(file, res);
-});
-
 
 app.listen(process.env.C9_PORT);
