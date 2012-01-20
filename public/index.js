2faa669b31aba98443c567572216a5500b4fec88
Mark Borcherding <markborcherding@gmail.com>
2012-01-19 21:06:53 -0600

    new other

--- public/index.js
+++ public/index.js
@@ -24,12 +24,31 @@ var states_for_user = {
   IA: 2,
   IL: 2 };
 
+var states_for_user_other = { 
+  AL: 0,
+  MS: 0,
+  LA: 0,
+  PA: 1,
+  NY: 1,
+  NJ: 1,
+  DE: 1,
+  MD: 1,
+  IN: 2,
+  MI: 2,
+  WI: 2,
+  MN: 2,
+  IA: 2,
+  IL: 2, 
+  MO: 2};
+
+var map;
 
 var svg = d3.select("#chart").append("svg")
     .attr("width", 960)
     .attr("height", 500);
 
 d3.json("us-states.json", function(json) {
+  map = json;
   var path = d3.geo.path();
 
   // A thick black stroke for the exterior.
@@ -52,32 +71,21 @@ d3.json("us-states.json", function(json) {
       });
       
     
-return;
-  // A white overlay to hide interior black strokes.
-  svg.append("g")
-      .attr("class", "white")
-    .selectAll("path")
-      .data(json.features)
-    .enter().append("path")
-      .attr("d", path);
-
-  // The polygons, scaled!
-  svg.append("g")
-      .attr("class", "grey")
-    .selectAll("path")
-      .data(json.features)
-    .enter().append("path")
-      .attr("d", path)
-      .attr("transform", function(d) {
-        var centroid = path.centroid(d),
-            x = centroid[0],
-            y = centroid[1];
-        return "translate(" + x + "," + y + ")"
-            + "scale(" + Math.sqrt(data[+d.id] * 5 || 0) + ")"
-            + "translate(" + -x + "," + -y + ")";
-      })
-      .style("stroke-width", function(d) {
-        return 1 / Math.sqrt(data[+d.id] * 5 || 1);
-      });
-
 });
+
+
+function other(){
+  d3.select("#chart g path")
+    .data(map.features)
+      .attr("class",function(d){
+          var state_name = d.properties.name;
+          var state =_.find(window.states_lookup.items, function(s){return s.name === state_name});
+          var user = states_for_user_other[state.abbreviation];
+          if(user != undefined){
+            console.log(state,users[user]);
+            return users[user];             
+          } else {
+              return "noone";
+          }
+      });
+}
